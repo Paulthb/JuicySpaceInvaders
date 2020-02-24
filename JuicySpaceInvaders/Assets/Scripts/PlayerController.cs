0fcbb927e7c4dca870bcce1990355d61ef4ec665
paul thibault <paulthb1@gmail.com>
2020-02-24 11:32:08 +0100

    add player move

---
+++ JuicySpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") == -1)
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Horizontal") == 1)
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Shoot") != 0)
+            Instantiate(bulletPrefab);
+    }
+}
