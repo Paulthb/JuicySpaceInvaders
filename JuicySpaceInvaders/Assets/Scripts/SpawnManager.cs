458cc9c5ef987b85a00c7af965a21fccd5955855
paul thibault <paulthb1@gmail.com>
2020-02-25 11:39:34 +0100

    add spawn list WIP

---
+++ JuicySpaceInvaders/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Spawn> SpawnList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemy()
+    {
+
+    }
+}
