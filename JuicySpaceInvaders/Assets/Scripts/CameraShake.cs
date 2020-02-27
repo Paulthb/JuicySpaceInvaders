5b31dd330d9bf5a58bf0ca6e94f1a807da06c74f
JeanDev <hardyjea@gmail.com>
2020-02-27 11:00:04 +0100

    add camera shake when enemy destroyed

---
+++ JuicySpaceInvaders/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Vector3 cameraInitialPosition;
+    public float shakeMagnitude = 0.05f, shakeTime = 0.5f;
+
+    private static CameraShake _instance;
+
+    public static CameraShake Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<CameraShake>();
+
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject("CameraShake");
+                    _instance = container.AddComponent<CameraShake>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+
+    public void ShakeIt()
+    {
+        cameraInitialPosition = this.transform.position;
+        InvokeRepeating("StartCameraShaking", 0f, 0.005f);
+        Invoke("StopCameraShaking", shakeTime);
+    }
+
+    void StartCameraShaking()
+    {
+        float cameraShakingOffsetX = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+        float cameraShakingOffsetY = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+        Vector3 cameraIntermediatePosition = this.transform.position;
+        cameraIntermediatePosition.x += cameraShakingOffsetX;
+        cameraIntermediatePosition.y += cameraShakingOffsetY;
+        this.transform.position = cameraIntermediatePosition;
+    }
+
+    void StopCameraShaking()
+    {
+        CancelInvoke("StartCameraShaking");
+        this.transform.position = cameraInitialPosition;
+    }
+}
